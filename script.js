6523310b5d7197601ad8652591122808fe0a7d9c
Danish <danishjrzehenvlog@gmail.com>
2022-12-18 20:47:00 +0530

    personal website
    Welcome to my personal website! My name is Danish Abdullah and I am a software developer with 0-1 years of experience in the industry. On this website, you will find information about my professional background, skills, and achievements

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select the nav element
+const nav = document.querySelector('nav');
+
+// Select the hamburger menu button
+const hamburger = document.querySelector('.hamburger');
+
+// Add a click event listener to the hamburger menu button
+hamburger.addEventListener('click', function() {
+  // Toggle the "open" class on the nav element
+  nav.classList.toggle('open');
+});
+
+{
+
+}
